15a1426394377a40de57e8cfc486ac5c5e10b7cb
Bertalan Krisztián <krisztian652@gmail.com>
2023-04-24 18:11:09 +0200

    Create popup dialog for phone number login, add back already existing registration/login and anonym login. redirect when entering mobile phone, and verify phone number.

---
+++ app/src/main/java/com/example/mystandardapp/ActivityVerification.java
@@ -0,0 +1,73 @@
+package com.example.mystandardapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+public class ActivityVerification extends AppCompatActivity {
+    private EditText editTextCode;
+    private Button buttonVerify;
+
+    private String verificationId;
+    private String resendingToken;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+
+        editTextCode = findViewById(R.id.editTextCode);
+        buttonVerify = findViewById(R.id.buttonVerifyCode);
+
+        SharedPreferences preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
+        verificationId = preferences.getString("verificationId", "");
+        resendingToken = preferences.getString("resendingToken", "");
+
+        buttonVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = editTextCode.getText().toString().trim();
+
+                if (TextUtils.isEmpty(code)) {
+                    Toast.makeText(ActivityVerification.this, "Please enter the verification code", Toast.LENGTH_SHORT).show();
+                } else {
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+                    signInWithPhoneAuthCredential(credential);
+                }
+            }
+        });
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // User sign in successful, start the main activity
+                            Intent intent = new Intent(ActivityVerification.this, packageList.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // Sign in failed, display a message to the user
+                            Toast.makeText(ActivityVerification.this, "Sign in failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    }
--- app/src/main/java/com/example/mystandardapp/MainActivity.java
+++ app/src/main/java/com/example/mystandardapp/MainActivity.java
@@ -2,22 +2,32 @@ package com.example.mystandardapp;
 
 import static androidx.core.content.PackageManagerCompat.LOG_TAG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText userNameET;
     EditText passwordET;
+    EditText phoneET;
+    private Button buttonVerify;
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +37,62 @@ public class MainActivity extends AppCompatActivity {
         userNameET = findViewById(R.id.editTextUserName);
         passwordET = findViewById(R.id.editTextPassword);
         mAuth = FirebaseAuth.getInstance();
+        buttonVerify = findViewById(R.id.mobileLoginButton);
+
+
     }
+
+    public void showMobileLoginDialog(View view) {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_mobile_login);
+        dialog.setCancelable(true);
+
+        final EditText editTextPhoneNumber = dialog.findViewById(R.id.editTextPhoneNumber);
+        final Button buttonLogin = dialog.findViewById(R.id.buttonLogin);
+
+
+        buttonLogin.setOnClickListener(v -> {
+            String phone = editTextPhoneNumber.getText().toString().trim();
+
+            if(TextUtils.isEmpty(phone)){
+                Toast.makeText(MainActivity.this, "Please enter a valid phone number", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                    phone,
+                    60,
+                    java.util.concurrent.TimeUnit.SECONDS,
+                    MainActivity.this,
+                    new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                        @Override
+                        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                            mAuth.signInWithCredential(phoneAuthCredential);
+                            Toast.makeText(MainActivity.this, "Verification completed", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onVerificationFailed(@NonNull FirebaseException e) {
+                            Toast.makeText(MainActivity.this, "Verification failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        @Override
+                        public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                            // Save the verification ID and resending token so we can use them later
+                            SharedPreferences preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
+                            preferences.edit().putString("verificationId", verificationId).apply();
+                            preferences.edit().putString("resendingToken", forceResendingToken.toString()).apply();
+
+                            // Start the verification activity to enter the code
+                            Intent intent = new Intent(MainActivity.this, ActivityVerification.class);
+                            startActivity(intent);
+                        }
+                    }
+            );
+        });
+        dialog.show();
+    }
+
+
+
 
     public void login(View view){
         String userName = userNameET.getText().toString();
@@ -49,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
 
                 });
     }
+
 
     public void redirectToMain(View view) {
         Intent intent = new Intent(this, packageList.class);
